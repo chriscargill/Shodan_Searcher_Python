fdf43d877eb86cbecf88c61bb4b2917ea7a87b42
Chris Cargill <chriscargill07@gmail.com>
2021-02-01 10:52:00 -0500

    Changed formatign from python2 to python3
    Changes to be committed:
    	modified:   shodansearch.py

--- shodansearch.py
+++ shodansearch.py
@@ -11,10 +11,10 @@ api = shodan.Shodan(SHODAN_API_KEY)
 try:	#Search Shodan
 	results = api.search(desired_search)
 	#Show the resultz
-	print 'Results found: %s' % results['total']
+	print('Results found: %s' % results['total'])
 	for result in results['matches']:
-	    print 'IP: %s' % result['ip_str'] # Print the ip address
-	    print result['data']
-        print 'END'
-except shodan.APIError, e:
-	print 'There was an error while searching Shodan: %s' % e
+		print('IP: %s' % result['ip_str']) # Print the ip address
+		print(result['data'])
+		print('END\n\n')
+except shodan.APIError as e:
+	print('There was an error while searching Shodan: %s' % e)
